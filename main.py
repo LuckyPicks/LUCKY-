936e7fce4627dfce18dd71838053bee9ef9896fc
LuckyPicks <travisjamelle@icloud.com>
2026-03-12 08:58:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import discord
-from discord.ext import commands
 import os
 
+TOKEN = os.getenv("DISCORD_TOKEN")
+
 intents = discord.Intents.default()
-intents.message_content = True
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
+@client.event
 async def on_ready():
-    print("LuckyPicks bot is online!")
+    print(f"{client.user} is online!")
 
-@bot.command()
-async def test(ctx):
-    await ctx.send("✅ LuckyPicks bot is working!")
-
-bot.run(os.getenv("DISCORD_TOKEN"))
+client.run(TOKEN)
