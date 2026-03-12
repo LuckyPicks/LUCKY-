8a3059b4acdc5a4783a4e714e786afe0333061a3
LuckyPicks <travisjamelle@icloud.com>
2026-03-12 08:24:21 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+
+client = commands.Bot(command_prefix="!", intents=intents)
+
+@client.event
+async def on_ready():
+    print("LuckyPicks Bot is online!")
+
+@client.command()
+async def test(ctx):
+    await ctx.send("✅ LuckyPicks bot is working!")
+
+client.run(os.getenv("DISCORD_TOKEN"))
