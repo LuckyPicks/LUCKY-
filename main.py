f433e24e424bdf390020beea6de1de3b45a865b5
LuckyPicks <travisjamelle@icloud.com>
2026-03-12 09:04:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,12 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f"{client.user} is online!")
+    for guild in client.guilds:
+        for channel in guild.text_channels:
+            try:
+                await channel.send("🤖 LuckyPicks bot is now online!")
+                return
+            except:
+                pass
 
 client.run(TOKEN)
