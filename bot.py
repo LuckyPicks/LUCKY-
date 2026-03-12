907acc7136c6e1bd0ea3e2140ff319accbec298e
LuckyPicks <travisjamelle@icloud.com>
2026-03-11 20:58:14 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,26 @@
 import discord
 import asyncio
+import os
+from discord.ext import commands
 
 from scheduler.update_loop import start_scanner
 
 intents = discord.Intents.default()
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
+    print(f"Logged in as {client.user}")
+
+    try:
+        synced = await client.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
 
     print("LuckyPicks Bot Online")
 
     asyncio.create_task(start_scanner())
-
-
-import os
 
 client.run(os.getenv("DISCORD_TOKEN"))
